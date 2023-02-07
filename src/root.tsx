1cd4a050ab9259ffb6cb2ccb717c385873691281
Filip Skoczen <philipskoczen@gmail.com>
2023-02-07 09:06:23 +0100

    Add favicon to topbar

--- src/root.tsx
+++ src/root.tsx
@@ -10,6 +10,7 @@ import {
   Routes,
   Scripts,
   Title,
+  Link,
 } from 'solid-start';
 
 export default function Root() {
@@ -19,6 +20,7 @@ export default function Root() {
         <Title>SolidStart - Bare</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
       <Body>
         <Suspense>
